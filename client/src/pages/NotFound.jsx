68c264c1f902ebfde95b84285ab2a0a0c28ab4eb
Haroon Saeed <haroon.saeed@outlook.com>
2025-12-29 15:24:18 +0100

    setup 404 page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <div>Page Not Found.</div>;
+  return (
+    <div
+      className="page-container"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h2>404</h2>
+      <p>Page Not Found.</p>
+      <Link to="/">Go to Home Page</Link>
+    </div>
+  );
 }
 
 export default NotFound;
