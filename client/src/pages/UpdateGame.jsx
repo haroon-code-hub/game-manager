84620cf7f3af2a07a86f89353df7c66281c423f1
Haroon Saeed <haroon.saeed@outlook.com>
2025-12-29 15:02:02 +0100

    add an update game funtionality

--- client/src/pages/UpdateGame.jsx
+++ client/src/pages/UpdateGame.jsx
@@ -1,5 +1,99 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+
 function UpdateGame() {
-  return <div>Update a game</div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  async function fetchGame() {
+    const response = await fetch(`http://localhost:3000/api/games/${id}`);
+    return response.json();
+  }
+  async function updateGame(updatedGame) {
+    const res = await fetch(`http://localhost:3000/api/games/${id}`, {
+      method: "PUT",
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify(updatedGame),
+    });
+    return res.json();
+  }
+
+  const { data, error, isLoading, isError } = useQuery({
+    queryKey: ["game", id],
+    queryFn: fetchGame,
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: updateGame,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["games"]);
+      queryClient.invalidateQueries(["game", id]);
+      navigate("/");
+    },
+  });
+  useEffect(() => {
+    if (data) {
+      reset({
+        name: data.name,
+        platform: data.platform,
+        genre: data.genre,
+      });
+    }
+  }, [data, reset]);
+  const onSubmit = (formData) => {
+    mutate({
+      name: formData.name,
+      platform: formData.platform,
+      genre: formData.genre,
+    });
+  };
+  if (isLoading) return <p className="page-container">Loading game...</p>;
+  if (isError) return <p className="page-container">Error: {error.message}</p>;
+  return (
+    <div className="page-container">
+      <h2>Update Game (ID:{id})</h2>
+      <form
+        className="form-container bg-card"
+        style={{ padding: "1rem" }}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label>Game Name</label>
+        <input
+          type="text"
+          placeholder="e.g. Super Mario"
+          {...register("name", { required: "Name is required." })}
+        />
+        {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
+        <label>Platform</label>
+        <input
+          type="text"
+          placeholder="e.g. Play Station, PC"
+          {...register("platform", { required: "Platform is required." })}
+        />
+        {errors.platform && (
+          <p style={{ color: "red" }}>{errors.platform.message}</p>
+        )}
+        <label>Genre</label>
+        <input
+          type="text"
+          placeholder="e.g. action, puzzle, racing"
+          {...register("genre", { required: "Genre is required." })}
+        />
+        {errors.genre && <p style={{ color: "red" }}>{errors.genre.message}</p>}
+
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  );
 }
 
 export default UpdateGame;
--- server/index.js
+++ server/index.js
@@ -10,11 +10,34 @@ app.get("/api/games", (req, res) => {
   res.json(games);
 });
 
+app.get("/api/games/:id", (req, res) => {
+  const { id } = req.params;
+  const game = games.find((g) => g.id === Number(id));
+  if (!game) return res.status(404).json({ error: "Game not found." });
+  return res.json(game);
+});
+
 app.post("/api/games", (req, res) => {
   const newGame = req.body;
   games.push({ ...newGame, id: games.length + 1 });
   res.status(201).json(newGame);
 });
+
+app.put("/api/games/:id", (req, res) => {
+  const { id } = req.params;
+  const newUpdatedGame = req.body;
+  const idx = games.findIndex((g) => g.id === Number(id));
+  if (idx === -1) return res.status(404).json({ error: "Game not found." });
+  games[idx] = {
+    ...games[idx],
+    name: newUpdatedGame.name,
+    platform: newUpdatedGame.platform,
+    genre: newUpdatedGame.genre,
+  };
+
+  res.json(games[idx]);
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
